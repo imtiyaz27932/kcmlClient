8c4cf48d5dce42a411afc0a2d29b06d2aade15c3
opc <imtiyaz.gw@gmail.com>
2025-11-10 12:12:56 +0000

    Update Login Test case about file path

--- Scripts/Login/Script1762593938445.groovy
+++ Scripts/Login/Script1762593938445.groovy
@@ -3,7 +3,8 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
@@ -17,7 +18,8 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Windows.startApplicationWithTitle('C:\\Users\\opc\\Desktop\\ImtiI.exe', 'KCML Client')
+filePath = RunConfiguration.getProjectDir() + "\\Resource\\ImtiI.exe"
+Windows.startApplicationWithTitle(filePath, 'KCML Client')
 
 Windows.click(findWindowsObject('Object Repository/Login/Password'))
 
